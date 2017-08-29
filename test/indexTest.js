acf1926ad1bcf7f9ab7218caf51bca8bb63c89cf
Alexi Rahman <alexi.rahman@r76.se>
2017-08-29 23:55:09 +0200

    Better structure in tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,25 +4,59 @@ const expect = require("chai").expect;
 const mapKeysDeep = require("../lib/index");
 
 describe(".mapKeysDeep()", () => {
-  expect(() => {
-    mapKeysDeep(undefined);
-  }).to.throw("map-keys-deep-lodash expects an object but got undefined");
-
-  const foo = mapKeysDeep({a: "b", c: "d", e: {c: "f", g: {c: "h"}}}, (value, key) => {
-    if (key === "c") {
-      return "zzz";
-    }
-    return key;
+  it("should throw exception when undefined", () => {
+    expect(() => {
+      mapKeysDeep(undefined);
+    }).to.throw("map-keys-deep-lodash expects an object but got undefined");
   });
 
-  assert.deepEqual(foo, {a: "b", zzz: "d", e: {zzz: "f", g: {zzz: "h"}}});
+  it("should return correct object with different subkeys", () => {
+    const foo = mapKeysDeep({
+      a: "b",
+      c: "d",
+      e: {
+        c: "f",
+        g: {
+          c: "h"
+        }
+      }
+    }, (value, key) => {
+      if (key === "c") return "zzz";
 
-  const bar = mapKeysDeep({a: {a: {a: "b"}}}, (value, key) => {
-    if (key === "a") {
-      return "zzz";
-    }
-    return key;
+      return key;
+    });
+
+    assert.deepEqual(foo, {
+      a: "b",
+      zzz: "d",
+      e: {
+        zzz: "f",
+        g: {
+          zzz: "h"
+        }
+      }
+    });
   });
 
-  assert.deepEqual(bar, {zzz: {zzz: {zzz: "b"}}});
+  it("should return correct object with same subkeys", () => {
+    const bar = mapKeysDeep({
+      a: {
+        a: {
+          a: "b"
+        }
+      }
+    }, (value, key) => {
+      if (key === "a") return "zzz";
+
+      return key;
+    });
+
+    assert.deepEqual(bar, {
+      zzz: {
+        zzz: {
+          zzz: "b"
+        }
+      }
+    });
+  });
 });
